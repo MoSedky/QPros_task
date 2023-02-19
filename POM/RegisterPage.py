62af0e50caa1e86236e1fd380ed547ecd0743ea4
mosedky <sedky.cs@gmail.com>
2023-02-19 17:17:45 +0100

    usage of GenericPage in RegisterPage.py

--- POM/RegisterPage.py
+++ POM/RegisterPage.py
@@ -4,12 +4,13 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
+from POM.GenericPage import GenericPage
 
-class RegisterPage:
+
+class RegisterPage(GenericPage):
 
     def __init__(self, driver):
-        self.driver = driver
-        self.wait = WebDriverWait(self.driver, 30)
+        super().__init__(driver)
 
     def insert_username(self, username):
         username_txt = self.wait.until(ec.visibility_of_element_located((By.XPATH,
@@ -26,11 +27,11 @@ class RegisterPage:
                                                                   '//button[contains(text(),"Sign")]')))
         sign_up_btn.click()
 
-    def click_accept_alert(self):
-        self.wait.until(ec.alert_is_present())
-        alert_text = self.driver.switch_to.alert.text
-        self.driver.switch_to.alert.accept()
-        return alert_text
+    # def click_accept_alert(self):
+    #     self.wait.until(ec.alert_is_present())
+    #     alert_text = self.driver.switch_to.alert.text
+    #     self.driver.switch_to.alert.accept()
+    #     return alert_text
 
     def click_close_btn_form(self):
         close_btn = self.wait.until(ec.element_to_be_clickable((By.XPATH,
