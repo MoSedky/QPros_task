957f78dfda72b0a5c14952cf221d1cf3774c3e37
mosedky <sedky.cs@gmail.com>
2023-02-19 17:12:58 +0100

    use GenericPage.py class driver in CartPage.py and Categories.py Pages

--- POM/CartPage.py
+++ POM/CartPage.py
@@ -5,12 +5,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 
+from POM.GenericPage import GenericPage
 
-class CartPage:
+
+class CartPage(GenericPage):
 
     def __init__(self, driver):
-        self.driver = driver
-        self.wait = WebDriverWait(self.driver, 30)
+        super().__init__(driver)
 
     def click_on_cart(self):
         cart_link = self.wait.until(ec.element_to_be_clickable((By.XPATH, '//a[contains(text(),"Cart")]')))
@@ -24,6 +25,70 @@ class CartPage:
     def click_place_order(self):
         place_order_btn = self.wait.until(ec.element_to_be_clickable((By.XPATH, '//button[text()="Place Order"]')))
         place_order_btn.click()
+
+    def check_place_order_form(self):
+        try:
+            self.wait.until(ec.visibility_of_all_elements_located
+                            ((By.XPATH, '//button[text()="Purchase"]/../..//input')))
+        except Exception as e:
+            pytest.fail(f'Place Order Form not displayed due to exception: {e}')
+
+    def insert_name(self, name):
+        name_txt = self.wait.until(ec.element_to_be_clickable((By.XPATH, '//input[@id="name"]')))
+        name_txt.send_keys(name)
+
+    def insert_country(self, country):
+        country_txt = self.wait.until(ec.element_to_be_clickable((By.XPATH, '//input[@id="country"]')))
+        country_txt.send_keys(country)
+
+    def insert_city(self, city):
+        city_txt = self.wait.until(ec.element_to_be_clickable((By.XPATH, '//input[@id="city"]')))
+        city_txt.send_keys(city)
+
+    def insert_credit_card(self, card):
+        card_txt = self.wait.until(ec.element_to_be_clickable((By.XPATH, '//input[@id="card"]')))
+        card_txt.send_keys(card)
+
+    def insert_month(self, month):
+        month_txt = self.wait.until(ec.element_to_be_clickable((By.XPATH, '//input[@id="month"]')))
+        month_txt.send_keys(month)
+
+    def insert_year(self, year):
+        year_txt = self.wait.until(ec.element_to_be_clickable((By.XPATH, '//input[@id="year"]')))
+        year_txt.send_keys(year)
+
+    def click_purchase_btn(self):
+        place_order_btn = self.wait.until(ec.element_to_be_clickable((By.XPATH, '//button[text()="Purchase"]')))
+        place_order_btn.click()
+
+    def click_close_form_btn(self):
+        place_order_btn = self.wait.until(ec.element_to_be_clickable(
+            (By.XPATH, '//button[text()="Purchase"]/..//button[text()="Close"]')))
+        place_order_btn.click()
+
+    def validate_success_purchase_alert(self):
+        try:
+            self.wait.until(ec.visibility_of_element_located((By.XPATH, '//*[contains(@class,"sweet-alert")]')))
+        except Exception as e:
+            pytest.fail(f'Purchase Order failed due to {e}')
+
+    def get_success_purchase_msg(self):
+        success_msg = self.wait.until(ec.visibility_of_element_located((By.XPATH,
+                                                                        '//*[contains(@class,"sweet-alert")]//h2')))
+        return success_msg.text
+
+    def get_purchase_msg_content(self):
+        success_msg_details = self.wait.until(ec.visibility_of_element_located
+                                              ((By.XPATH, '//*[contains(@class,"sweet-alert")]//p')))
+        return success_msg_details.text
+
+    def get_order_total(self):
+        total_txt = self.wait.until(ec.visibility_of_element_located((By.XPATH, '//label[contains(text(),"Total")]')))
+        return total_txt.text[7:]
+
+    def click_purchase_ok(self):
+        okay_btn = self.wait.until(ec.visibility_of_element_located((By.XPATH, '//button[text()="OK"]')))
+        okay_btn.click()
 
     def click_delete_category(self):
         delete_btn = self.wait.until(ec.element_to_be_clickable((By.XPATH, '//a[contains(text(),"Delete")]')))
--- POM/Categories.py
+++ POM/Categories.py
@@ -3,12 +3,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 
+from POM.GenericPage import GenericPage
 
-class CategoriesPage:
+
+class CategoriesPage(GenericPage):
 
     def __init__(self, driver):
-        self.driver = driver
-        self.wait = WebDriverWait(self.driver, 30)
+        super().__init__(driver)
 
     def get_list_categories(self):
         category_items = self.wait.until(ec.visibility_of_all_elements_located((By.XPATH,
@@ -27,14 +28,10 @@ class CategoriesPage:
     def click_on_rand_category(self):
         category_items = self.wait.until(ec.visibility_of_all_elements_located((By.XPATH, '//a[@class="hrefch"]')))
         rand_item = random.choice(category_items)
+        item_txt = rand_item.text
         rand_item.click()
+        return item_txt
 
     def click_add_to_cart(self):
         item = self.wait.until(ec.element_to_be_clickable((By.XPATH, '//a[contains(text(),"cart")]')))
         item.click()
-
-    def check_click_accept_alert(self):
-        self.wait.until(ec.alert_is_present())
-        alert_text = self.driver.switch_to.alert.text
-        self.driver.switch_to.alert.accept()
-        return alert_text
