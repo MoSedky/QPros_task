7bf97913cd8b30e0c24c2bdea77df9cf1d8bb6ea
mosedky <sedky.cs@gmail.com>
2023-02-19 17:46:01 +0100

    add username and password to rand_str.csv

--- Workers/helper_methods.py
+++ Workers/helper_methods.py
@@ -6,14 +6,14 @@ import re
 
 def generate_random_string():
     generated_rand_str = ''.join(random.choices(string.ascii_lowercase, k=5))
-    save_generated_str(generated_rand_str)
+    save_generated_str(str(generated_rand_str))
     return generated_rand_str
 
 
 def save_generated_str(rand_str):
-    f = open('Resources/rand_str.csv', 'w')
+    f = open('Resources/rand_str.csv', 'a')
     writer = csv.writer(f)
-    writer.writerow(rand_str)
+    writer.writerow([f'username_{rand_str}', f'password_{rand_str}'])
     f.close()
 
 
