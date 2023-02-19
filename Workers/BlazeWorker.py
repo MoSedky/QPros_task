aaa118776163eb29a9bfc367bd987bab94ba82d9
mosedky <sedky.cs@gmail.com>
2023-02-19 17:12:27 +0100

    add place_order method in BlazeWorker.py, add_rand_item method, call main_page

--- Workers/BlazeWorker.py
+++ Workers/BlazeWorker.py
@@ -6,7 +6,7 @@ from POM.LoginPage import LoginPage
 from POM.RegisterPage import RegisterPage
 from constants import MAIN_URL, category_names
 from POM.MainPage import MainPage
-from helper_methods import generate_random_string
+from Workers.helper_methods import generate_random_string, get_purchase_values, get_current_date
 
 
 class BlazeWorker:
@@ -17,11 +17,14 @@ class BlazeWorker:
     def validate_main_page(self, expected_logo_txt):
         main_page = MainPage(self.driver)
         logo_text = main_page.assert_web_logo()
-        assert logo_text == expected_logo_txt, f'Text in Logo different that expected. Current Text: {logo_text}' \
-                                               f'Expected Text: {expected_logo_txt} '
+        assert logo_text.replace(" ", "") == expected_logo_txt.replace(" ", ""), \
+            f'Text in Logo different than expected. Current Text: {logo_text}' \
+            f'Expected Text: {expected_logo_txt} '
         logging.info('Main Page opens successfully')
 
     def validate_sign_up(self, expected_alert_txt):
+        main_page = MainPage(self.driver)
+        main_page.click_register_btn()
         register_page = RegisterPage(self.driver)
         random_text = generate_random_string()
         register_page.insert_username(f'username_{random_text}')
@@ -34,17 +37,19 @@ class BlazeWorker:
         return random_text
 
     def validate_login(self, rand_txt):
+        main_page = MainPage(self.driver)
+        main_page.click_login_btn()
         login_page = LoginPage(self.driver)
         login_page.insert_username(f'username_{rand_txt}')
         login_page.insert_password(f'password_{rand_txt}')
         login_page.click_login_btn_form()
         logged_in_username = login_page.get_logged_in_username()
-        assert logged_in_username == f'username_{rand_txt}', f'Displayed logged in Username is not as expected. ' \
-                                                             f'Current LoggedIn: {logged_in_username}' \
-                                                             f'Expected Text: username_{rand_txt} '
+        assert logged_in_username[8:] == f'username_{rand_txt}', f'Displayed logged in Username is not as expected. ' \
+                                                                 f'Current LoggedIn: {logged_in_username[8:]}' \
+                                                                 f'Expected Text: username_{rand_txt} '
         logging.info(f'User: username_{rand_txt} logged in successfully')
 
-    def check_categories_items(self):
+    def check_select_categories_items(self):
         categories_page = CategoriesPage(self.driver)
         list_categories = categories_page.get_list_categories()
         list_categories.sort()
@@ -57,11 +62,14 @@ class BlazeWorker:
             assert len(items_in_category) > 1, f'Item list in category: {category} is empty'
 
         logging.info(f'All Categories have items as expected')
-        logging.info('Now adding an item randomly to be added to the cart')
-        categories_page.click_on_rand_category()
+
+    def add_rand_item(self):
+        categories_page = CategoriesPage(self.driver)
+        item_title = categories_page.click_on_rand_category()
         categories_page.click_add_to_cart()
-        success_msg = categories_page.check_click_accept_alert()
+        success_msg = categories_page.click_accept_alert()
         assert success_msg == 'Product added.', 'Error while attempting to add a product'
+        return item_title
 
     def open_cart_page(self):
         cart_page = CartPage(self.driver)
@@ -75,3 +83,38 @@ class BlazeWorker:
                                         f'Expected: {item_name} != Current: {item_title}'
         cart_page.click_delete_category()
         cart_page.validate_item_deleted()
+
+    def place_order(self, item_name, name, country, city, card_no, month, year):
+        cart_page = self.open_cart_page()
+        item_title = cart_page.get_category_title()
+        assert item_title == item_name, 'Mismatch between displayed item name and item title selected.' \
+                                        f'Expected: {item_name} != Current: {item_title}'
+        cart_page.click_place_order()
+        cart_page.check_place_order_form()
+        cart_page.insert_name(name)
+        cart_page.insert_country(country)
+        cart_page.insert_city(city)
+        cart_page.insert_credit_card(card_no)
+        cart_page.insert_month(month)
+        cart_page.insert_year(year)
+        cart_page.click_purchase_btn()
+        success_msg = cart_page.get_success_purchase_msg()
+        current_total = cart_page.get_order_total()
+        current_date = get_current_date()
+        assert success_msg == 'Thank you for your purchase!', f'An error in purchase success msg.Current: {success_msg}'
+        content_msg = cart_page.get_purchase_msg_content()
+        final_msg = content_msg.replace('\n', '')
+        id_val, amount_val, card_no_val, name_val, date_val = get_purchase_values(final_msg.strip())
+        assert name_val == name, f'Different Name displayed than inserted in purchasing Order. ' \
+                                 f'Expected: {name}!= Current: {name_val}'
+        assert card_no_val == card_no, f'Different Card Number displayed than inserted in purchasing Order. ' \
+                                       f'Expected: {name}!= Current: {name_val}'
+
+        assert current_total == amount_val, f'Different Amount displayed than inserted in purchasing Order. ' \
+                                            f'Expected: {current_total}!= Current: {amount_val}'
+
+        assert current_date == date_val, f'Different Date displayed than inserted in purchasing Order. ' \
+                                         f'Expected: {current_date}!= Current: {date_val}'
+
+        cart_page.click_purchase_ok()
+        logging.info('Order Purchased successfully')
